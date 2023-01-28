0ce0d8cda36781efa876c04a38f5547e9d229bb0
LokeshKanagaraj10 <120785781+LokeshKanagaraj10@users.noreply.github.com>
2023-01-29 01:41:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ function letterCombinations(input_digit) {
   //console.log(arr)
   const alp =["0","1","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"]
   const ans = arr.map(num => alp[num])
+if(ans.length == 2){
   let i
   let j
   const fans = []
@@ -14,6 +15,23 @@ function letterCombinations(input_digit) {
     }
   }
   return fans
+}
+	else if(ans.length == 3){
+		 let i
+		let j
+		let k
+  const fans = []
+  for(i=0;i<ans[0].length;i++)
+  {
+    for(j=0;j<ans[1].length;j++)
+    {
+		for(k=0;k<ans[2].length;k++){
+      fans.push((ans[0][i] + ans[1][j] + ans[2][k]))
+		}
+    }
+  }
+  return fans
+	}
 }
 
 module.exports = letterCombinations;
